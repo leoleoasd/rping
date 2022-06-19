755b0858a34eb743d3477d2a18fb977833b25689
Leo Lu <luyuxuanleo@gmail.com>
2022-06-19 16:45:02 +0800

    Ping

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,38 @@
 use pnet::datalink::{self, NetworkInterface};
 use pnet::datalink::Channel::Ethernet;
+use pnet::packet::icmp::echo_request::{IcmpCodes, EchoRequest, self};
+use pnet::packet::icmp::{Icmp, IcmpTypes, checksum, IcmpPacket};
 use pnet::packet::{Packet, MutablePacket};
 use pnet::packet::ethernet::{EthernetPacket, MutableEthernetPacket};
-
+use pnet::transport::{TransportChannelType::*, transport_channel, icmp_packet_iter};
+use pnet::transport::TransportProtocol::*;
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::util;
 use std::env;
+use std::error::Error;
+use std::net::IpAddr;
 
 // Invoke as echo <interface name>
-fn main() {
-    let interface_name = env::args().nth(1).unwrap();
-    let interface_names_match =
-        |iface: &NetworkInterface| iface.name == interface_name;
+fn main() -> Result<(), Box<dyn Error>> {
+    let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Icmp));
+    let (mut tx, mut rx) = transport_channel(4096, protocol).unwrap();
+    // Allocate enough space for a new packet
+    let mut vec: Vec<u8> = vec![0; 16];
 
-    // Find the network interface with the provided name
-    let interfaces = datalink::interfaces();
-    let interface = interfaces.into_iter()
-                              .filter(interface_names_match)
-                              .next()
-                              .unwrap();
+    // Use echo_request so we can set the identifier and sequence number
+    let mut echo_packet = echo_request::MutableEchoRequestPacket::new(&mut vec[..]).unwrap();
+    echo_packet.set_sequence_number(20);
+    echo_packet.set_identifier(2);
+    echo_packet.set_icmp_type(IcmpTypes::EchoRequest);
 
-    // Create a new channel, dealing with layer 2 packets
-    let (mut tx, mut rx) = match datalink::channel(&interface, Default::default()) {
-        Ok(Ethernet(tx, rx)) => (tx, rx),
-        Ok(_) => panic!("Unhandled channel type"),
-        Err(e) => panic!("An error occurred when creating the datalink channel: {}", e)
-    };
-
-    loop {
-        match rx.next() {
-            Ok(packet) => {
-                let packet = EthernetPacket::new(packet).unwrap();
-
-                // Constructs a single packet, the same length as the the one received,
-                // using the provided closure. This allows the packet to be constructed
-                // directly in the write buffer, without copying. If copying is not a
-                // problem, you could also use send_to.
-                //
-                // The packet is sent once the closure has finished executing.
-                tx.build_and_send(1, packet.packet().len(),
-                    &mut |mut new_packet| {
-                        let mut new_packet = MutableEthernetPacket::new(new_packet).unwrap();
-
-                        // Create a clone of the original packet
-                        new_packet.clone_from(&packet);
-
-                        // Switch the source and destination
-                        new_packet.set_source(packet.get_destination());
-                        new_packet.set_destination(packet.get_source());
-                });
-            },
-            Err(e) => {
-                // If an error occurs, we can handle it here
-                panic!("An error occurred while reading: {}", e);
-            }
-        }
+    let csum = util::checksum(echo_packet.packet(), 1);
+    echo_packet.set_checksum(csum);
+    tx.set_ttl(128).unwrap();
+    tx.send_to(echo_packet, "10.0.0.2".parse::<IpAddr>().unwrap())?;
+    let mut it = icmp_packet_iter(&mut rx);
+    while let Ok((p, a)) = it.next() {
+        println!("{p:?} {a:?}");
+        println!("{:?}", p.payload());
     }
+    Ok(())
 }
